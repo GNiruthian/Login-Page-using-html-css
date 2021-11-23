7e55d813e2712f8c67d56c4b9f2a396cc7f0c389
G.Niruthian <88297426+GNiruthian@users.noreply.github.com>
2021-11-23 05:47:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+const form = document.querySelector("form");
+eField = form.querySelector(".email"),
+    eInput = eField.querySelector("input"),
+    pField = form.querySelector(".password"),
+    pInput = pField.querySelector("input");
+
+form.onsubmit = (e) => {
+    e.preventDefault();
+
+    (eInput.value == "") ? eField.classList.add("shake", "error"): checkEmail();
+    (pInput.value == "") ? pField.classList.add("shake", "error"): checkPass();
+
+    setTimeout(() => {
+        eField.classList.remove("shake");
+        pField.classList.remove("shake");
+    }, 500);
+
+    eInput.onkeyup = () => { checkEmail(); }
+    pInput.onkeyup = () => { checkPass(); }
+
+    function checkEmail() {
+        let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+        if (!eInput.value.match(pattern)) {
+            eField.classList.add("error");
+            eField.classList.remove("valid");
+            let errorTxt = eField.querySelector(".error-txt");
+
+            (eInput.value != "") ? errorTxt.innerText = "Enter a valid email address": errorTxt.innerText = "Email can't be blank";
+        } else {
+            eField.classList.remove("error");
+            eField.classList.add("valid");
+        }
+    }
+
+    function checkPass() {
+        if (pInput.value == "") {
+            pField.classList.add("error");
+            pField.classList.remove("valid");
+        } else {
+            pField.classList.remove("error");
+            pField.classList.add("valid");
+        }
+    }
+
+    if (!eField.classList.contains("error") && !pField.classList.contains("error")) {
+        window.location.href = form.getAttribute("action");
+    }
+}
